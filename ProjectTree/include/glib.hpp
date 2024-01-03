083796aa6e2ad8b1786d905afd5be7ad4f85f70b
Oleg Burmistr <ok.burmistr@gmail.com>
2024-01-03 09:33:16 +0200

    Add a thread and mutex

---
+++ ProjectTree/include/glib.hpp
@@ -0,0 +1,22 @@
+
+
+#ifndef GLIB_HPP
+#define GLIB_HPP
+
+   #ifdef __cplusplus
+      extern "C"
+      {
+   #endif
+
+
+#include"glib.h"
+#include"glib-object.h"
+#include"gio.h"
+
+   #ifdef __cplusplus
+      } // extern "C"
+   #endif
+
+
+
+#endif /* GLIB_HPP */
--- ProjectTree/src/main.cpp
+++ ProjectTree/src/main.cpp
@@ -1,5 +1,17 @@
 #include<iostream>
+
+#include"glib.hpp"
+
 #include"test.hpp"
+
+#define LOAD		(1000u)
+
+
+uint32_t SharedResurse = 0ul;
+
+void Thread1_Func(void);
+
+GMutex *mutex;
 
 int main()
 {
@@ -7,6 +19,35 @@ int main()
 	std::cout << "Hello, Student" << std::endl;
 	result = CalcSumm(50, 40);
 	std::cout << "Sum is: " << result << std::endl;
-	return 0;
 
+
+
+	mutex = g_new(GMutex, 1);
+	g_mutex_init(mutex);
+	GThread * Thread1 = g_thread_new(NULL, (GThreadFunc) Thread1_Func, NULL);
+
+	for(uint32_t i = 0u; i < LOAD; i++)
+	{
+		g_mutex_lock(mutex);
+		SharedResurse++;
+		std::cout << "MainThread:" << SharedResurse << std::endl;
+		g_mutex_unlock(mutex);
+	}
+	
+	g_thread_join(Thread1);
+
+	return 0;
+}
+
+
+void Thread1_Func(void)
+{
+
+	for(uint32_t i = 0u; i < LOAD; i++)
+	{		
+		g_mutex_lock(mutex);
+		SharedResurse++;
+		std::cout << "Thread1:" << SharedResurse << std::endl;
+		g_mutex_unlock(mutex);
+	}
 }

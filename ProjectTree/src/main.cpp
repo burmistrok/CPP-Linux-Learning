3bc58ed94c1d0486f955b4ab1dbf28cf5fba006c
Oleg Burmistr <ok.burmistr@gmail.com>
2024-01-07 23:34:41 +0200

    Impliment template for new task

--- ProjectTree/src/main.cpp
+++ ProjectTree/src/main.cpp
@@ -1,29 +1,157 @@
 #include<iostream>
+#include <cstdint>
+
 
 #include"glib.hpp"
 
-#include"test.hpp"
+#define LOAD		(1000u)
+uint32_t SharedResurse = 0ul;
+void Thread1_Func( gpointer data );
+GMutex *mutex;
 
 
-int main(int argc, char *argv[])
+typedef enum{
+	MAIN_PROCESS = 0,
+	CHILD_PROCESS,
+	TEST_THREAD,
+	TEST_THREADANDMUTEX,
+
+	UNSUPORTED_GOAL
+}TE_GOAL;
+
+const gchar ua8_ChildProcess[] = "CHILD";
+const gchar ua8_Thread[] = "THREADS";
+const gchar ua8_ThreadMx[] = "THREADS_MUTEX";
+void MainProcess(void);
+void ChildProcess(void);
+void TestMultiThread(uint8_t useMutex);
+
+TE_GOAL FindGoal(const char* Flag);
+
+int main( int argc, char **argv )
 {
-	GSubprocess * childProcess = NULL;
-	GError * lerror = NULL;
-	const gchar * const largv[] = { "\./PrintMsg.elf", NULL };
-	GSubprocessLauncher *handlelaunch = NULL;
 
-	std::cout << "Process " << getpid() << " started" << std::endl;
-
-
-	handlelaunch = g_subprocess_launcher_new((GSubprocessFlags)(G_SUBPROCESS_FLAGS_STDOUT_PIPE | G_SUBPROCESS_FLAGS_STDERR_MERGE));
-
-	std::cout << "g_subprocess_launcher_new passed "<< std::endl;
-
-	childProcess = g_subprocess_launcher_spawn ( handlelaunch, &lerror, (const gchar*)largv, NULL );
-
-	if(lerror != NULL)
-	{
-		g_error ("Spawning child failed: %s", lerror->message);
-		return 1;
+	int RetVal = 0;
+	std::cout << "The argc is: " << argc <<std::endl;
+	if(1 == argc)
+	{/* Main process */
+		MainProcess();
 	}
+	else
+	{
+		if(2 == argc)
+		{
+			TE_GOAL lcase = FindGoal(argv[1]);
+			switch(lcase)
+			{
+				case CHILD_PROCESS:
+					ChildProcess();
+				break;
+
+				case TEST_THREAD:
+					TestMultiThread(0u);
+				break;
+
+				case TEST_THREADANDMUTEX:
+					TestMultiThread(1u);
+				break;
+
+				default:
+					std::cout << "Flag is not supported." << std::endl;
+					RetVal = 2;
+			}
+		}
+		else
+		{
+			std::cout << "Incorect no of flags." << std::endl;
+			RetVal = 1;
+		}
+	}
+
+	return RetVal;
+}
+
+void MainProcess(void)
+{
+	std::cout << "Main process was called." << std::endl;
+}
+
+void ChildProcess(void)
+{
+	std::cout << "Child process was called." << std::endl;
+}
+
+void TestMultiThread(uint8_t useMutex)
+{
+	if(0u != useMutex )
+	{
+	mutex = g_new(GMutex, 1);
+	g_mutex_init(mutex);
+	}
+	GThread * Thread1 = g_thread_new(NULL, (GThreadFunc)Thread1_Func, &useMutex);
+
+	for(uint32_t i = 0u; i < LOAD; i++)
+	{
+		if(0u != useMutex )
+		{
+			g_mutex_lock(mutex);
+		}
+		SharedResurse++;
+		std::cout << "MainThread:" << SharedResurse << std::endl;
+		if(0u != useMutex )
+		{
+			g_mutex_unlock(mutex);
+		}
+	}
+	
+	g_thread_join(Thread1);
+}
+
+void Thread1_Func( gpointer data)
+{
+
+	uint8_t useMutex = *((uint8_t*)data);
+
+	for(uint32_t i = 0u; i < LOAD; i++)
+	{
+		if(0u != useMutex )
+		{
+			g_mutex_lock(mutex);
+		}
+		SharedResurse++;
+		std::cout << "Thread1:" << SharedResurse << std::endl;
+		if(0u != useMutex )
+		{
+			g_mutex_unlock(mutex);
+		}
+	}
+}
+
+TE_GOAL FindGoal(const char* Flag)
+{
+	TE_GOAL retVal = UNSUPORTED_GOAL;
+	GString* sFlag = g_string_new((const gchar*) Flag);
+
+	sFlag = g_string_ascii_up(sFlag);
+
+	GString* ls_ChildProcess = g_string_new(ua8_ChildProcess);
+	GString* ls_Thread = g_string_new(ua8_Thread);
+	GString* ls_ThreadMx = g_string_new(ua8_ThreadMx);
+
+	if(g_string_equal(sFlag, ls_ChildProcess) )
+	{
+		retVal = CHILD_PROCESS;
+	}
+	else if( g_string_equal(sFlag, ls_Thread) )
+	{
+		retVal = TEST_THREAD;
+	}
+	else if( g_string_equal(sFlag, ls_ThreadMx) )
+	{
+		retVal = TEST_THREADANDMUTEX;
+	}
+	else {/* Misra */}
+
+	return retVal;
+
 }
--- ProjectTree/src/test.cpp
+++
@@ -1,7 +0,0 @@
-#include"test.hpp"
-
-
-uint32_t CalcSumm(uint32_t a, uint32_t b)
-{
-	return (a + b);
-}

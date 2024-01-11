0220f16aad0569d1cd30b554b7fb197310840197
Oleg Burmistr <ok.burmistr@gmail.com>
2024-01-12 00:36:18 +0200

    Backup of working version of IPC via sockets

--- ProjectTree/src/main.cpp
+++ ProjectTree/src/main.cpp
@@ -1,24 +1,15 @@
 #include<iostream>
 #include <cstdint>
 #include"C_HeaderFiles.hpp"
-
 /* Defines */
 #define LOAD					(1000u)
 #define LOCAL_BUFFER_SIZE		(1000u)
 #define CUSTMPORT 				(9000u)
 #define HOSTNAME 				"127.0.0.1"
 #define MSG_TO_SEND 			"Hello from 2024, Happy new year!!! Update the status. Let's go on holidays to The Universitare Psychiatrische Dienste (UPD)."
+#define CANCELE_TIMEOUT			(10)
 
 /* User-defined data type  */
-
-typedef enum{
-	MAIN_PROCESS = 0,
-	CHILD_PROCESS,
-	TEST_THREAD,
-	TEST_THREADANDMUTEX,
-
-	UNSUPORTED_GOAL
-}TE_GOAL;
 
 typedef enum{
 	NONE = 0,
@@ -28,9 +19,7 @@ typedef enum{
 
 typedef struct{
 	TE_CONNECTION conectionType;
-	GSocketConnection* connectionPtr;
-	GInputStream* istreamPtr;
-	GOutputStream* ostreamPtr;
+	GSocket* connectionPtr;
 }TS_CONNECITON;
 
 typedef struct{
@@ -44,14 +33,19 @@ void ChildProcess(void);
 void TestMultiThread(uint8_t useMutex);
 void Thread1_Func( gpointer data );
 gboolean OpenSocket(TS_CONNECITON* connPtr);
-gboolean WriteToSocket(GOutputStream* ostreamptr, guint8* data, gsize size);
-void ReadFromSocket(GInputStream* istreamptr, guint8* data, gsize size, gsize* readBytes);
-GSocketConnection* CreateServer(void);
-GSocketConnection* Connect2Server(void);
+gboolean WriteToSocket(GSocket* socketPtr, guint8* data, gsize size);
+void ReadFromSocket(GSocket* socketPtr, guint8* data, gsize size, gsize* readBytes);
+GSocket* CreateServer(void);
+GSocket* Connect2Server(void);
 void ChildBackend(void);
 void ParentBackend(void);
 guint CountStrinStr(GString* str, gchar* find);
+gpointer CancAtTimeout_thread (gpointer data);
+static char *socket_address_to_string (GSocketAddress *address);
+gpointer CancConAtTimeout_thread (gpointer data);
 
+
+static gboolean bConnetctionTimeout = FALSE;
 static gboolean b_UseSocket = FALSE;
 static gboolean b_UseTread = FALSE;
 static gboolean b_UseTreadWMx = FALSE;
@@ -60,7 +54,7 @@ static GOptionEntry entries[] =
 {
 	{ "UseSocket", 'S', 0, G_OPTION_ARG_NONE, &b_UseSocket, "Transmit data through sockets", NULL },
 	{ "UseThread", 'T', 0, G_OPTION_ARG_NONE, &b_UseTread, "The threads increment and print the same global variable", NULL },
-	{ "UseThreadWithMutex", 'M', 0, G_OPTION_ARG_NONE, &b_UseTread, "The threads increment and print the same global variable", NULL },
+	{ "UseThreadWithMutex", 'M', 0, G_OPTION_ARG_NONE, &b_UseTread, "The threads increment and print the same global variable protected with mutex", NULL },
 	{ NULL }
 };
 
@@ -139,7 +133,7 @@ void MainProcess(void)
 {
 
 	std::cout << "Main " << getpid() <<" process was called" << std::endl;
-
+	
 	if( OpenSocket(&s_clientConn) )
 	{
 		std::cout << "Conection created successfully, in " << getpid() <<" process(main)." << std::endl;
@@ -147,6 +141,7 @@ void MainProcess(void)
 		ParentBackend();
 	}
 
+	g_socket_close(s_clientConn.connectionPtr, NULL);
 	g_object_unref(s_clientConn.connectionPtr);
 }
 
@@ -161,6 +156,7 @@ void ChildProcess(void)
 		ChildBackend();
 
 	}
+	g_socket_close(serverConn.connectionPtr, NULL);
 	g_object_unref(serverConn.connectionPtr);
 }
 
@@ -214,7 +210,7 @@ gboolean OpenSocket(TS_CONNECITON* connPtr)
 {
 
 	gboolean RetVal = FALSE;
-	GSocketConnection* lConectionPtr = NULL;
+	GSocket* lConectionPtr = NULL;
 
 	if( SERVER_SIDE == connPtr->conectionType )
 	{
@@ -231,73 +227,124 @@ gboolean OpenSocket(TS_CONNECITON* connPtr)
 	if(NULL != lConectionPtr )
 	{
 		connPtr->connectionPtr = lConectionPtr;
-		connPtr->istreamPtr = g_io_stream_get_input_stream(G_IO_STREAM(lConectionPtr));
-		connPtr->ostreamPtr = g_io_stream_get_output_stream(G_IO_STREAM(lConectionPtr));
 		RetVal = TRUE;
 	}
 	return RetVal;
 }
 
-GSocketConnection* CreateServer(void)
+GSocket* CreateServer(void)
 {
-	GError * lerror = NULL;
-	GSocketConnection* new_connectionPtr = NULL;
-	GSocketListener *lListnSocketPtr = g_socket_listener_new();
-	if(NULL == lListnSocketPtr)
-	{
-		g_error ("Could not create server socket.");
+	GError *lerror = NULL;
+	GCancellable *lcancellablePtr = NULL;
+	GSocketAddress *lSocketAdrPtr = NULL;
+	GSocketAddress *lClinetAdrPtr = NULL;
+	GInetAddress *lInetAdrPtr = NULL;
+	GSocket* lnewsocketptr = NULL;
+	GSocket* lsocketptr = g_socket_new (G_SOCKET_FAMILY_IPV4, G_SOCKET_TYPE_STREAM, G_SOCKET_PROTOCOL_TCP, &lerror);
+	if(NULL != lerror){
+		g_warning ("Could not create server socket %s", lerror->message);
 	}
-	else
-	{
+	else{
+		lInetAdrPtr = g_inet_address_new_any(G_SOCKET_FAMILY_IPV4);
+		lSocketAdrPtr = g_inet_socket_address_new(lInetAdrPtr, CUSTMPORT);
 		g_clear_error (&lerror);
-		g_socket_listener_add_inet_port(lListnSocketPtr, (guint16 )CUSTMPORT, NULL, &lerror);
-
-		if(lerror != NULL)
-		{
-			g_error ("Could not add listner: %s", lerror->message);
+		/* normally allow_reuse should be TRUE for server sockets in order to not get G_IO_ERROR_ADDRESS_IN_USE*/
+		if( FALSE == g_socket_bind (lsocketptr, lSocketAdrPtr, TRUE, &lerror)){
+			g_warning ("Can't bind socket: %s\n", lerror->message);
 		}
-		else
-		{
+		else{
 			g_clear_error (&lerror);
-			new_connectionPtr = g_socket_listener_accept(lListnSocketPtr, NULL, NULL, &lerror);
-			if(lerror != NULL)
+			/* Ensure that after timeout a listen funciton will be terminated */
+			if(FALSE == g_socket_listen ( lsocketptr, &lerror) ){
+				if(NULL != lerror){
+					g_warning ("Can't listen on the socket: %s\n", lerror->message);
+				}
+			}
+			else{
+				g_clear_error (&lerror);
+				/* Ensure that after timeout a listen funciton will be terminated */
+				if( FALSE == g_socket_condition_timed_wait(lsocketptr, G_IO_IN, (G_USEC_PER_SEC * CANCELE_TIMEOUT), NULL, &lerror ) ){
+					if(NULL != lerror){
+						g_warning ("Timeout ocurred, and no client connected: %s\n", lerror->message);
+					}
+				}
+				else{
+					lcancellablePtr = g_cancellable_new ();
+					g_thread_new(NULL, CancAtTimeout_thread, lcancellablePtr);
+					g_clear_error (&lerror);
+					lnewsocketptr = g_socket_accept (lsocketptr, lcancellablePtr, &lerror);
+					if(NULL != lerror)
+					{
+						g_warning ("Can't accept the client: %s\n", lerror->message);
+					}
+					else{
+						g_clear_error (&lerror);
+						lClinetAdrPtr = g_socket_get_remote_address (lnewsocketptr, &lerror);
+						if(NULL != lerror){
+							g_warning("Error getting remote address: %s\n", lerror->message);
+						}
+						else{
+							std::cout << "Got a new connection from(server): " << socket_address_to_string(lClinetAdrPtr) << std::endl;
+						}
+						g_object_unref(lClinetAdrPtr);
+					}
+				}
+			}
+				
+		}
+		g_object_unref(lInetAdrPtr);
+	}
+	g_object_unref (G_OBJECT (lsocketptr));
+	return lnewsocketptr;
+}
+
+
+GSocket* Connect2Server(void)
+{
+	GSocketAddress *lClinetAdrPtr = NULL;
+	GInetAddress *lInetAdrPtr = NULL;
+	GSocket* lnewsocketptr = NULL;
+	GError * lerror = NULL;
+	gboolean lbConnected = FALSE;
+	lInetAdrPtr = g_inet_address_new_from_string(HOSTNAME);
+	lClinetAdrPtr = g_inet_socket_address_new (lInetAdrPtr, CUSTMPORT);
+	g_thread_new(NULL, CancConAtTimeout_thread, NULL);
+	while( (FALSE == bConnetctionTimeout) && (FALSE == lbConnected) )
+	{
+		lnewsocketptr = g_socket_new (G_SOCKET_FAMILY_IPV4, G_SOCKET_TYPE_STREAM, G_SOCKET_PROTOCOL_TCP, &lerror);
+		if(NULL != lerror){
+			g_warning ("Could not create client socket %s", lerror->message);
+		}else{
+			g_clear_error (&lerror);
+			if(FALSE == g_socket_connect ( lnewsocketptr, lClinetAdrPtr, NULL, &lerror))
 			{
-				g_error ("Could not create new conection: %s", lerror->message);
-				new_connectionPtr = NULL;
-				g_socket_listener_close(lListnSocketPtr);
-				g_object_unref (lListnSocketPtr);
+				if(NULL != lerror){
+					g_warning ("Could not connect client socket %s", lerror->message);
+				}
+				g_clear_error (&lerror);
+				g_socket_close (lnewsocketptr, &lerror);
+				if(NULL != lerror){
+					g_error ("Could not close client socket %s", lerror->message);
+				}
+				g_object_unref (G_OBJECT (lnewsocketptr));
+				g_clear_error (&lerror);
+			}
+			else{
+				lbConnected = TRUE;
+				std::cout << "Got a new connection from(client): " << socket_address_to_string(lClinetAdrPtr) << std::endl;
 			}
 		}
-	}
-	g_socket_listener_close(lListnSocketPtr);
-	g_object_unref (lListnSocketPtr);
 
-	return new_connectionPtr;
+	}
+	g_object_unref(lInetAdrPtr);
+	return lnewsocketptr;
 }
 
-GSocketConnection* Connect2Server(void)
-{
-	GError * lerror = NULL;
-	GSocketConnection* new_connectionPtr = NULL;
-	//sleep(1);
-	GSocketClient* lClientSocketPtr  = g_socket_client_new ();
-	new_connectionPtr = g_socket_client_connect_to_host ( lClientSocketPtr, HOSTNAME, CUSTMPORT , NULL,  &lerror);
-	if(lerror != NULL)
-	{
-		g_error ("Could not connect to host: %s", lerror->message);
-		new_connectionPtr = NULL;
-		g_object_unref (lClientSocketPtr);
-	}
-
-	g_object_unref (lClientSocketPtr);
-	return new_connectionPtr;
-}
-
-gboolean WriteToSocket(GOutputStream* ostreamptr, guint8* data, gsize  size)
+gboolean WriteToSocket(GSocket* socketPtr, guint8* data, gsize  size)
 {
 	gboolean retVal = FALSE;
 	GError * lerror = NULL;
-	gssize writtenbytes = g_output_stream_write (ostreamptr, data, size, NULL, &lerror);
+	gssize writtenbytes = g_socket_send (socketPtr, (gchar*)data, size, NULL, &lerror);
 
 	if(lerror != NULL)
 	{
@@ -314,10 +361,10 @@ gboolean WriteToSocket(GOutputStream* ostreamptr, guint8* data, gsize  size)
 	return retVal;
 }
 
-void ReadFromSocket(GInputStream* istreamptr, guint8* data, gsize size, gsize* readBytes)
+void ReadFromSocket(GSocket* socketPtr, guint8* data, gsize size, gsize* readBytes)
 {
 	GError * lerror = NULL;
-	gssize lreadBytes = g_input_stream_read(istreamptr, data, size, NULL, &lerror);
+	gssize lreadBytes = g_socket_receive(socketPtr, (gchar*)data, size, NULL, &lerror);
 	if(lerror != NULL)
 	{
 		g_error ("Error occured during reading: %s", lerror->message);
@@ -335,7 +382,7 @@ void ChildBackend(void)
 {
 	guint8 receivedMsg[LOCAL_BUFFER_SIZE];
 	gsize receivedBytes = 0;
-	ReadFromSocket(serverConn.istreamPtr , receivedMsg, LOCAL_BUFFER_SIZE, &receivedBytes);
+	ReadFromSocket(serverConn.connectionPtr , receivedMsg, LOCAL_BUFFER_SIZE, &receivedBytes);
 
 	if(0 != receivedBytes)
 	{
@@ -343,27 +390,30 @@ void ChildBackend(void)
 	}
 
 	GString* lsMsg = g_string_new((const gchar*) receivedMsg);
-
-/*g_regex_replace */
-	gchar lfind[2] = "0";
-	gchar lreplase = '\0';
-	for( gchar sym = '0'; sym <= '9'; sym++ )
-	{
-		lfind[0] = sym;
-		g_string_replace (lsMsg, lfind, &lreplase, 0);
+	GError * lerror = NULL;
+	GRegex *cleanup = g_regex_new ("[0-9]", G_REGEX_DEFAULT, G_REGEX_MATCH_DEFAULT, &lerror);
+	if(NULL != lerror){
+		g_error ("Error occured during regex creation: %s", lerror->message);
 	}
+	g_clear_error (&lerror);
+	gchar* ls_result = g_regex_replace (cleanup, (gchar*)receivedMsg, strlen((char*)receivedMsg), 0, "\0", G_REGEX_MATCH_DEFAULT , &lerror);
+	if(NULL != lerror){
+		g_error ("Error occured during regex replace: %s", lerror->message);
+	}
+	g_regex_unref(cleanup);
 
-	if (WriteToSocket(serverConn.ostreamPtr, (guint8*)lsMsg->str, ((gsize)lsMsg->len)+1))
+	if (WriteToSocket(serverConn.connectionPtr, (guint8*)ls_result, (strlen((char*)ls_result)+1)))
 	{
 		std::cout << "Server returned the message" << std::endl;
 	}
+	g_free(ls_result);
 
 	guint lNoOfFind;
 	gchar lfindStr[] = "UPD";
 
 	lNoOfFind = CountStrinStr(lsMsg, lfindStr);
 
-	if( WriteToSocket(serverConn.ostreamPtr, (guint8*)&lNoOfFind, sizeof(guint)) )
+	if( WriteToSocket(serverConn.connectionPtr, (guint8*)&lNoOfFind, sizeof(guint)) )
 	{
 		std::cout << "Server returned the message" << std::endl;
 	}
@@ -377,13 +427,13 @@ void ParentBackend(void)
 	guint8 receivedMsg[LOCAL_BUFFER_SIZE];
 	gsize receivedBytes = 0;
 	GString* Msg = g_string_new((gchar*)MSG_TO_SEND);
-	if (WriteToSocket(s_clientConn.ostreamPtr, (guint8*)Msg->str, ((gsize)Msg->len)+1))
+	if (WriteToSocket(s_clientConn.connectionPtr, (guint8*)Msg->str, ((gsize)Msg->len)+1))
 	{
 		std::cout << "Client sent msg successfully" << std::endl;
 	}
 	
 
-	ReadFromSocket(s_clientConn.istreamPtr , receivedMsg, LOCAL_BUFFER_SIZE, &receivedBytes);
+	ReadFromSocket(s_clientConn.connectionPtr , receivedMsg, LOCAL_BUFFER_SIZE, &receivedBytes);
 	if(0 != receivedBytes)
 	{
 		std::cout << "Returned message is:" << std::endl << receivedMsg << std::endl;
@@ -395,7 +445,7 @@ void ParentBackend(void)
 	lNoOfFind = CountStrinStr(Msg, lfind);
 	std::cout << "String \"upd\" was found: "<< lNoOfFind << " times" << std::endl;
 
-	ReadFromSocket(s_clientConn.istreamPtr , (guint8*)&lNoOfFind, sizeof(guint), &receivedBytes);
+	ReadFromSocket(s_clientConn.connectionPtr , (guint8*)&lNoOfFind, sizeof(guint), &receivedBytes);
 	if(0 != receivedBytes)
 	{
 		std::cout << "Server found string \"upd\": "<< lNoOfFind << " times" << std::endl;
@@ -410,4 +460,38 @@ guint CountStrinStr(GString* str, gchar* find)
 {
 	str = g_string_ascii_up(str);
 	return g_string_replace (str, find, find, 0);
+}
+
+
+gpointer CancAtTimeout_thread (gpointer data)
+{
+  GCancellable *cancellable = (GCancellable*)data;
+  std::cout << "Thread called" << std::endl;
+  g_usleep (G_USEC_PER_SEC*CANCELE_TIMEOUT);
+  std::cout << "Timeout ocurred" << std::endl;
+  g_cancellable_cancel (cancellable);
+  return NULL;
+}
+
+gpointer CancConAtTimeout_thread (gpointer data)
+{
+  std::cout << "Thread2 called" << std::endl;
+  g_usleep (G_USEC_PER_SEC*CANCELE_TIMEOUT);
+  std::cout << "Timeout2 ocurred" << std::endl;
+  bConnetctionTimeout = TRUE;
+  return NULL;
+}
+
+static char *socket_address_to_string (GSocketAddress *address)
+{
+  GInetAddress *inet_address;
+  char *str, *res;
+  int port;
+
+  inet_address = g_inet_socket_address_get_address (G_INET_SOCKET_ADDRESS (address));
+  str = g_inet_address_to_string (inet_address);
+  port = g_inet_socket_address_get_port (G_INET_SOCKET_ADDRESS (address));
+  res = g_strdup_printf ("%s:%d", str, port);
+  g_free (str);
+  return res;
 }
